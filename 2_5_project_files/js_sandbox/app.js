4fd8d9158a03d3c08485cf70cc8a82095e9ee609
Thomas Bongiorno <tbongiorno1@gmail.com>
2020-06-30 12:49:22 -0400

    6.30

--- 2_5_project_files/js_sandbox/app.js
+++ 2_5_project_files/js_sandbox/app.js
@@ -35,5 +35,5 @@ const val1 = String(5);
 const val2 = 6;
 const sum = Number(val1 + val2);
 
-console.log(sum);
-console.log(typeof sum);
+// console.log(sum);
+// console.log(typeof sum);
---
+++ 2_5_project_files/js_sandbox/sandbox.js
@@ -0,0 +1,6 @@
+let arr = [];
+
+for (i = 0; i < 20; i++) {
+  arr.push(i);
+  console.log(typeof(arr[i]))
+}

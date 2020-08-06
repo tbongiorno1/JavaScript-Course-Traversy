7b6e9cda42c88a9b9fe7a5abfc66ec123679f637
Thomas Bongiorno <tbongiorno1@gmail.com>
2020-08-06 09:32:12 -0400

    8.5

--- 7_13_project_files/easyhttp2/app.js
+++ 7_13_project_files/easyhttp2/app.js
@@ -1,6 +1,6 @@
 const http = new EasyHTTP;
 
-// Get Users
+//Get users
 // http.get('https://jsonplaceholder.typicode.com/users')
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
@@ -8,21 +8,21 @@ const http = new EasyHTTP;
 // User Data
 const data = {
   name: 'John Doe',
-  username: 'johndoe',
+  useranme: 'johndoe',
   email: 'jdoe@gmail.com'
 }
 
-// Create User
+// create post
 // http.post('https://jsonplaceholder.typicode.com/users', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
 
-// Update Post
+// create put
 // http.put('https://jsonplaceholder.typicode.com/users/2', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
 
-// Delete User
+// delete user
 http.delete('https://jsonplaceholder.typicode.com/users/2')
-.then(data => console.log(data))
-.catch(err => console.log(err));
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
--- 7_13_project_files/easyhttp2/easyhttp2.js
+++ 7_13_project_files/easyhttp2/easyhttp2.js
@@ -1,32 +1,32 @@
-/**
- * EasyHTTP Library
- * Library for making HTTP requests
- *
+/** 
+ * Easy http library
+ * library for making http requests
+ * 
  * @version 2.0.0
- * @author  Brad Traversy
+ * @author TJ Bongiorno
  * @license MIT
- *
- **/
+ * 
+**/
 
- class EasyHTTP {
-   
-  // Make an HTTP GET Request 
+class EasyHTTP {
+  // make hhtp GET request
   get(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url)
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-    });
+      return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+      });
+   
   }
 
-  // Make an HTTP POST Request
+  // make POST request
   post(url, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
         method: 'POST',
         headers: {
-          'Content-type': 'application/json'
+          'Content-type': ' application/json'
         },
         body: JSON.stringify(data)
       })
@@ -34,39 +34,35 @@
       .then(data => resolve(data))
       .catch(err => reject(err));
     });
-  }
+}
 
-   // Make an HTTP PUT Request
-   put(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-    });
-  }
-
-  // Make an HTTP DELETE Request
-  delete(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: 'DELETE',
-        headers: {
-          'Content-type': 'application/json'
-        }
-      })
-      .then(res => res.json())
-      .then(() => resolve('Resource Deleted...'))
-      .catch(err => reject(err));
-    });
-  }
-
- }
-
- 
+// make a PUT request
+put(url, data) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': ' application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => resolve(data))
+    .catch(err => reject(err));
+  });
+}
+// Delete
+delete(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': ' application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(() => resolve('Resource Deleted'))
+    .catch(err => reject(err));
+  });
+}
+};
